ca974a708ac40a70ea460430c87030035cd7b34b
Nesma-Mostafa <34743979+Nesma-Mostafa@users.noreply.github.com>
2018-04-26 14:05:19 +0200

    Update Normalize.py

--- Scripts/Normalize.py
+++ Scripts/Normalize.py
@@ -1,18 +1,19 @@
-count=0
-factor=0
-data=[]
-with open ("/home/nesma/Documents/RNA_Kmers-master/hh/nesma.fa",'r') as f:
-	data =f.read().splitlines()
-	for i in range(0,len(data),2):
-		count+=int(data[i].split('>')[1])
-        factor=1000000/count
-
-for i in range(0,len(data),2):
-	data[i]= '>'+ str(int(data[i].split('>')[1])*factor)
-print(data)
-
-with open ("/home/nesma/Documents/RNA_Kmers-master/hh/nesma.fa",'w') as f:
-	f.truncate()
-        for i in range(0,len(data),1):
-		f.write(data[i]+'\n')
-f.close()
+import os
+WorkDir = os.getcwd()
+for filename in os.listdir(WorkDir):
+	count=0
+	factor=0
+	data=[]
+	if(len(filename.split('.'))==2 and filename.split('.')[1]=="fa"):
+		with open (WorkDir+'/'+filename,'r') as f:
+			data =f.read().splitlines()
+			for i in range(0,len(data),2):
+				count+=int(data[i].split('>')[1])
+			factor=1000000/count
+		for i in range(0,len(data),2):
+			data[i]= '>'+ str(int(data[i].split('>')[1])*factor)
+		with open ("/home/nesma/Documents/RNA_Kmers-master/hh/nesma.fa",'w') as f:
+			f.truncate()
+		        for i in range(0,len(data),1):
+		                f.write(data[i]+'\n')
+		f.close()
